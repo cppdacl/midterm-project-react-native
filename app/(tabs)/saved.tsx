c3b5b51bf2b5057f13e5b28d0eb9aeaae56c4666
cppdecl <axljohnmendoza@gmail.com>
2026-02-21 10:21:05 +0800

    Fixed an issue with saved tab having a space underneath
    Signed-off-by: cppdecl <axljohnmendoza@gmail.com>

--- app/(tabs)/saved.tsx
+++ app/(tabs)/saved.tsx
@@ -28,7 +28,7 @@ export default function SavedScreen() {
   const toggleTheme = () => setDark(!dark);
 
   return (
-    <SafeAreaView style={{ backgroundColor: theme.background, flex: 1 }}>
+    <SafeAreaView style={{ backgroundColor: theme.background }}>
       <StatusBar
         barStyle={dark ? "light-content" : "dark-content"}
         backgroundColor={theme.background}
